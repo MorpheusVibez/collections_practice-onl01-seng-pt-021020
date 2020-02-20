31adad318321ba110449b2c60ba2325c3fa8abb2
Andre Skeene <halodre24@gmail.com>
2020-02-20 01:58:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -33,5 +33,11 @@ def kesha_maker(str)
 end
 
 def find_a(str)
-  str.start_with?("a")
+  def my_find(collection)
+  i = 0 
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+  i += 1 
+  end
+end
 end
