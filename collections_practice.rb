da24d9347fe3c0299b709b22985159080a23a747
Andre Skeene <halodre24@gmail.com>
2020-02-20 01:26:02 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,27 @@
+def sort_array_asc(int)
+  int.sort
+end
 
+def sort_array_desc(int)
+  int.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(str)
+  str.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(int)
+  int.sort.reverse
+  int[1], int[2] = int[2], int[1], int[0]
+end
+
+def kesha_maker(str)
+  str.collect
